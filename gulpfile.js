2c6a055ec7f12f351f9831865c529c5df099bb36
David <Adam>
2014-08-06 22:26:11 +0800

    Show case browserify
    browserify manage jquery, mustache….automatically as plugins.

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,8 @@
 var gulp = require('gulp'),
     gutil = require('gulp-util'),
     coffee = require('gulp-coffee'),
-    concat = require('gulp-concat');
+    concat = require('gulp-concat'),
+    browserify = require('gulp-browserify');
 
 // gulp.task('log', function(){
 //   gutil.log('Workflows are awesome');
@@ -25,5 +26,6 @@ gulp.task('coffee', function(){
 gulp.task('js', function(){
     gulp.src(jsSources)
         .pipe(concat('script.js'))
+        .pipe(browserify())
         .pipe(gulp.dest('builds/development/js'))
 });
