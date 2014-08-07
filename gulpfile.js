87fbc51b087217eb3942428d1bf96bcf2f56eebf
David <Adam>
2014-08-07 20:58:38 +0800

    Add live reload for json and static
    live reload for json and static.

--- gulpfile.js
+++ gulpfile.js
@@ -18,6 +18,8 @@ var jsSources = [
     'components/scripts/template.js'
 ];
 var sassSource = ['components/sass/style.scss'];
+var htmlSources = ['builds/development/*.html'];
+var jsonSources = ['builds/development/js/*.json'];
 
 gulp.task('coffee', function(){
     gulp.src(coffeeSources)
@@ -50,6 +52,8 @@ gulp.task('watch', function(){
     gulp.watch(coffeeSources, ['coffee']);
     gulp.watch(jsSources, ['js']);
     gulp.watch('components/sass/*.scss', ['compass']);
+    gulp.watch(htmlSources, ['html']);
+    gulp.watch(jsonSources, ['json']);
 });
 
 gulp.task('connect', function(){
@@ -59,4 +63,14 @@ gulp.task('connect', function(){
     });
 });
 
-gulp.task('default', ['coffee','js', 'compass', 'connect', 'watch']);
+gulp.task('html', function(){
+    gulp.src(htmlSources)
+        .pipe(connect.reload());
+});
+
+gulp.task('json', function(){
+    gulp.src(jsonSources)
+        .pipe(connect.reload());
+});
+
+gulp.task('default', ['html', 'json', 'coffee','js', 'compass', 'connect', 'watch']);
